f382fa4d7cd37d63c1e1e89fc4db9601763d9344
KyrunaKJ <kyrunakj@gmail.com>
2023-09-16 13:36:34 -0500

    Added fps_counter files

---
+++ src/fps_counter.h
@@ -0,0 +1,8 @@
+#ifndef FPS_COUNTER_H
+#define FPS_COUNTER_H
+
+#include <GLFW/glfw3.h>
+
+void update_fps_counter(GLFWwindow*);
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "shader_reader.h"
 #include "main_program.h"
 #include "shape.h"
+#include "fps_counter.h"
 
 using namespace std;
 
@@ -14,8 +15,6 @@ using namespace std;
 #define DEBUG 1
 
 // cd cmake .. && make && ./test
-
-void update_fps_counter(GLFWwindow*);
 
 int main() {
     
@@ -85,20 +84,4 @@ int main() {
     LOG("Window Closed");
 
     return 0;
-}
-
-void update_fps_counter(GLFWwindow* window) {
-    static double previous_seconds = glfwGetTime();
-    static int frame_count;
-    double current_seconds = glfwGetTime();
-    double elapsed_seconds = current_seconds - previous_seconds;
-    if (elapsed_seconds > 0.25) {
-        previous_seconds = current_seconds;
-        double fps = (double)frame_count / elapsed_seconds;
-        char tmp[128];
-        sprintf(tmp, "opengl @fps: %.2f", fps);
-        glfwSetWindowTitle(window, tmp);
-        frame_count = 0;
-    }
-    frame_count++;
 }
